8100b9c2ea339be49df868477056023bdb3c7cfd
Leander Lismond <Leo2807@users.noreply.github.com>
2018-02-25 22:56:15 +0100

    Update

---
+++ src/components/Evolution.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="evolution">
+    <h2>{{title}}</h2>
+    <TwentyTwenty :before="from" :after="to" />
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+import TwentyTwenty from 'vue-twentytwenty'
+
+export default {
+  components: {
+    TwentyTwenty
+  },
+  props: ['from', 'to', 'text', 'title']
+}
+</script>
+
+<style>
+.evolution {
+  border: 0.5rem solid #4DBA87;
+  margin: 1rem;
+  overflow: clip;
+}
+</style>
