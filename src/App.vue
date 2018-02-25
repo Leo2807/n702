bcd165a0baeea7cb97a410816f7126b26d92c565
Leander Lismond <Leander@Lismond.be>
2018-02-25 23:31:18 +0100

    JK

--- src/App.vue
+++ src/App.vue
@@ -34,34 +34,34 @@ export default {
       },
       evolutions: [
         {
-          from: '2017.jpg',
-          to: '1971.png',
+          from: '2017',
+          to: '1971',
           text: 'In 1971 zie je dat de N702 nog niet doorgetrokken was. Daarnaast is het Kapermolenpark nog niet aangelegd en is er weinig bebouwing langs de R71.',
           title: '1971'
         },
         {
-          from: '1971.png',
-          to: '1979-1990.jpg',
+          from: '1971',
+          to: '1979-1990',
           text: 'In de jaren ‘80 is de N702 al doorgetrokken naar de kleine ring, de R70, die toen nog een 2×2-rijstrookconfiguratie had. Men is ook al begonnen aan de grenslandhallen. Het kapermolenpark en -zwembad zijn ook al aangelegd. Tot slot is er ook meer bebouwing aan de buitenkant van de grote ring.',
           title: '1979-1990'
         },
         {
-          from: '1979-1990.jpg',
-          to: '2000-2003.png',
+          from: '1979-1990',
+          to: '2000-2003',
           title: '2000-2003',
           text: 'Tegen 2000-2003 zijn de grenslandhallen met één gebouw uitgebreid, is de Japanse tuin ook aangelegd. Op de foto kan je het niet goed zien, maar de kleine ring is omgebouwd tot een eenrichtingsstraat met twee rijstroken.'
         },
         {
-          from: '2000-2003.png',
-          to: '2017.jpg',
+          from: '2000-2003',
+          to: '2017',
           title: '2017',
           text: 'Tegen 2017 zijn de grenslandhallen uitgebreid met de Ethiasarena en Plopsaland. De N702 wordt nu voor de helft als parkeerstrook gebruikt waardoor er nog een 2×1-weg overblijft. Het Kapermolenzwembad is verkleind, en op de vrijgekomen plaats is nu een bouwwerf aanwezig.'
         }
       ].map((ev) => {
         return {
           ...ev,
-          from: `/static/img/geopunt/Geopunt Vlaanderen ${ev.from}`,
-          to: `/static/img/geopunt/Geopunt Vlaanderen ${ev.to}`
+          from: require('./assets/geopunt/Geopunt Vlaanderen ' + ev.from + '.png'),
+          to: require('./assets/geopunt/Geopunt Vlaanderen ' + ev.to + '.png')
         }
       })
     }
